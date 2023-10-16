3eedbce9cb96f7dfdbb90849fb679934519b7648
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-10-17 03:16:36 +0800

    crud app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = 'flash message'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '392002'
 app.config['MYSQL_DB'] = 'crud_flask_app'
 
 mysql = MySQL(app)
